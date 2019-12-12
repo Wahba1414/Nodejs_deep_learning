3de76a5929aba5b553d3cd5ce1666c81767e51b0
Zizo1414 <zizo@makeitsimple.co.uk>
2019-12-12 16:42:22 +0200

    zlib-zip-unzip files

---
+++ zlib/unzip_file.js
@@ -0,0 +1,9 @@
+const zlib = require('zlib');
+const fs = require('fs');
+
+var unzip = zlib.createGunzip();
+
+var readStream = fs.createReadStream('./file1.zip');
+var writeStream = fs.createWriteStream('./file2.txt');
+
+readStream.pipe(unzip).pipe(writeStream);
---
+++ zlib/zip_file.js
@@ -0,0 +1,10 @@
+const zlib = require('zlib');
+const fs = require('fs');
+
+var zip = zlib.createGzip();
+
+var readStream = fs.createReadStream('./file1.txt');
+var writeStream = fs.createWriteStream('./file1.zip');
+
+
+readStream.pipe(zip).pipe(writeStream);
